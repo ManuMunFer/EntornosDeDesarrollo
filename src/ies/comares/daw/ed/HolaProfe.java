e6e24aebd01c17b394a5f9b03f7f843c0ee00e0f
Mario Romero <mario@fifa.es>
2024-10-22 12:54:41 +0200

    subida mario romero

--- src/ies/comares/daw/ed/HolaProfe.java
+++ src/ies/comares/daw/ed/HolaProfe.java
@@ -3,13 +3,11 @@ package ies.comares.daw.ed;
 public class HolaProfe {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Hola, mi nombre es Manu Muñoz");
 
 		System.out.println("Hola, mi nombre es Mario Romero");
-		
 
-		
 		System.out.println("Hola, mi nombre es Antonio Rodríguez");
 	}
 
